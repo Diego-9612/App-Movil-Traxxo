e91bcd6cc236db5d894247aa03b6f451719e6a9a
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2025-04-19 16:56:50 -0500

    Creamos nustro primer modulo llamado usuarios

--- Backend_Djando_Traxxo/Backend_Djando_Traxxo/settings.py
+++ Backend_Djando_Traxxo/Backend_Djando_Traxxo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Backend_Djando_Traxxo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'traxxo_db',
+        'USER': 'root',
+        'PASSWORD': '96122325463',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
---
+++ Backend_Djando_Traxxo/users/models.py
@@ -0,0 +1,16 @@
+from django.db import models # type: ignore
+
+# Create your models here.
+# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    image = models.CharField(max_length=255, null=True, blank=False)
+    password = models.CharField(max_length=255)
+    notification_token = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
